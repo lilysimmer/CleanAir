94cb40be018c010997e21139e05d181be182cd36
lilysimmer <carmenhajek2004@gmail.com>
2026-04-25 17:22:20 -0500

    Initial commit - Carmen

---
+++ TF/src/test/java/pe/edu/upc/tf/TfApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
